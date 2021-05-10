925b014799b5c2f467c3c46de34d2ba3f53ddd0b
el0xren <el0xren@gmail.com>
2021-05-10 06:44:19 +0300

    Misc changes in A50.sh

--- A50.sh
+++ A50.sh
@@ -9,8 +9,8 @@ export ANDROID_MAJOR_VERSION=r
 export PLATFORM_VERSION=11.0.0
 
 # Export KBUILD flags
-export KBUILD_BUILD_USER=neel0210
-export KBUILD_BUILD_HOST=hell
+export KBUILD_BUILD_USER=el0xren
+export KBUILD_BUILD_HOST=heaven
 
 # CCACHE
 export CCACHE="$(which ccache)"
@@ -23,27 +23,27 @@ export CROSS_COMPILE=$(pwd)/gcc/bin/aarch64-linux-android-
 export CLANG_TRIPLE=$(pwd)/clang/bin/aarch64-linux-gnu-
 export CC=$(pwd)/clang/bin/clang
 
-sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install \
-          bison build-essential curl flex git gnupg gperf \
-          liblz4-tool libncurses5-dev libsdl1.2-dev libxml2 \
-          libxml2-utils lzop pngcrush schedtool \
-          squashfs-tools xsltproc zip zlib1g-dev \
-          build-essential kernel-package libncurses5-dev \
-          bzip2 git python expect \
-          gcc-aarch64-linux-gnu g++-aarch64-linux-gnu -y
+#sudo apt-get update
+#sudo DEBIAN_FRONTEND=noninteractive apt-get install \
+#          bison build-essential curl flex git gnupg gperf \
+#          liblz4-tool libncurses5-dev libsdl1.2-dev libxml2 \
+#          libxml2-utils lzop pngcrush schedtool \
+#          squashfs-tools xsltproc zip zlib1g-dev \
+#          build-essential kernel-package libncurses5-dev \
+#          bzip2 git python expect \
+#          gcc-aarch64-linux-gnu g++-aarch64-linux-gnu -y
 
 #
-curl https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/android_build_env.sh | bash
+#curl https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/android_build_env.sh | bash
 
 # Check if have gcc/32 & clang folder
-if [ ! -d "$(pwd)/gcc/" ]; then
-   git clone --depth 1 git://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 gcc
-fi
+#if [ ! -d "$(pwd)/gcc/" ]; then
+#   git clone --depth 1 git://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 gcc
+#fi
 
-if [ ! -d "$(pwd)/clang/" ]; then
-   git clone --depth 1 https://github.com/PrishKernel/toolchains.git -b proton-clang12 clang
-fi
+#if [ ! -d "$(pwd)/clang/" ]; then
+#   git clone --depth 1 https://github.com/PrishKernel/toolchains.git -b proton-clang12 clang
+#fi
 
 echo "======================="
 echo "Making kernel with ZIP"
@@ -57,7 +57,6 @@ rm ./PRISH/AIK/image-new.img
 rm ./PRISH/AIK/ramdisk-new.cpio.empty
 rm ./PRISH/AIK/split_img/boot.img-zImage
 rm ./PRISH/AK/Image
-rm ./PRISH/ZIP/PRISH/NXT/boot.img
 rm ./PRISH/ZIP/PRISH/A50/boot.img
 rm ./PRISH/ZIP/PRISH/D/A50/boot.img
 rm ./PRISH/AK/*.zip
@@ -65,7 +64,7 @@ clear
 echo "======================="
 echo "Making kernel with ZIP"
 echo "======================="
-make A50_defconfig
+make a50_defconfig
 make -j64
 echo "Kernel Compiled"
 echo ""
@@ -75,12 +74,12 @@ echo "======================="
 echo ""
 cp -r ./arch/arm64/boot/Image ./PRISH/AIK/split_img/boot.img-zImage
 cp -r ./arch/arm64/boot/Image ./PRISH/AK/Image
-./PRISH/AIK/repackimg.sh
+. PRISH/AIK/repackimg.sh
 cp -r ./PRISH/AIK/image-new.img ./PRISH/ZIP/PRISH/D/A50/boot.img
 cd PRISH/ZIP
 echo "==========================="
 echo "Packing into Flashable zip"
 echo "==========================="
-./zip.sh
+. zip.sh
 cd ../..
-cp -r ./PRISH/ZIP/1.zip ./output/PrishKernel-ONEUI-R4-A50dd.zip
+cp -r ./PRISH/ZIP/1.zip ./output/CarbonKernel-A50-1.0.zip
